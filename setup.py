113f8a51466eb06cf511f954ef34444cb77ef269
Tim Simpson <tim.simpson@rackspace.com>
2012-07-09 17:45:03 -0500

    Bumping version to 1.2.5.3.
    * Fixing bug in runs_after_groups inheritance.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(fname):
 
 setup(
     name="proboscis",
-    version="1.2.5.2",
+    version="1.2.5.3",
     author='Rackspace',
     author_email='tim.simpson@rackspace.com',
     description="Extends Nose with certain TestNG like features.",
