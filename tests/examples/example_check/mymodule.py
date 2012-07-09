c684da1b3761e25a9d4ddf62cb7af6778bc9d828
Tim Simpson <tim.simpson@rackspace.com>
2012-07-09 16:47:59 -0500

    Adding an example for check.

---
+++ tests/examples/example_check/mymodule.py
@@ -0,0 +1,99 @@
+import random
+
+database = None
+web_server = None
+
+class AuthException(Exception):
+    pass
+
+class UserNotFoundException(Exception):
+    pass
+
+
+def create_database():
+    global database
+    database = { 'users':{} }
+
+def destroy_database():
+    global database
+    database = None
+
+
+def get_admin_client():
+    return UserServiceClient(0, { 'username':"admin", 'password':None })
+
+def login(user_settings):
+    for user in database['users'].values():
+        if user.username == user_settings['username']:
+            if user.password != user_settings['password']:
+                raise UserNotFoundException()
+            else:
+                return user
+    raise UserNotFoundException()
+
+def reverse(string):
+    """Reverses a string."""
+    return string[::-1]
+
+def start_web_server():
+    global web_server
+    web_server = {
+        "bob": {
+            "credentials":"admin",
+            "image":"default.jpg"
+        }
+    }
+
+    
+def bad_start_web_server():
+    raise RuntimeError("Error starting service.")
+
+
+def stop_web_server():
+    global web_server
+    web_server = None
+
+
+def tables_exist():
+    return database != None
+
+
+class UserServiceClient(object):
+
+    def __init__(self, id, settings):
+        self.id = id
+        self.username = settings['username']
+        self.password = settings['password']
+        self.type = settings.get('type', 'normal')
+
+    @property
+    def check_credentials(self):
+        return web_server[self.username]["credentials"]
+
+    def create_user(self, settings):
+        if self.id != 0 and self.type != 'admin':
+            raise AuthException("Must be an Admin to perform this function.")
+        random.seed()
+        id = random.randint(1, 1000)
+        settings['id'] = id
+        user = UserServiceClient(id, settings)
+        database['users'][id] = user
+        web_server[user.username] = {'image':"default.jpg"}
+        return user
+
+    def delete_user(self, id):
+        if self.id != 0 and self.type != 'admin':
+           raise AuthException("Must be an Admin to perform this function.")
+        if id not in database['users']:
+            raise UserNotFoundException()
+        del database['users'][id]
+
+
+    def get_profile_image(self):
+        return web_server[self.username]["image"]
+
+    def set_profile_image(self, new_value):
+        web_server[self.username]["image"] = new_value
+
+    def service_is_up(self):
+        return web_server != None
---
+++ tests/examples/example_check/tests/service_tests.py
@@ -0,0 +1,168 @@
+"""
+User service tests.
+
+This is a test for a fictitious user web service which has rich client bindings
+written in Python.
+
+It assumes we have an existing test database which we can run the web service
+against, using the function "mymodule.start_web_server()."
+
+After spinning up the service, the test creates a new user and tests various
+CRUD actions. Since its a test database, it is OK
+to leave old users in the system but we try to always delete them if possible
+at the end of the test.
+
+"""
+
+from datetime import datetime
+import random
+import types
+import unittest
+import mymodule
+from proboscis.asserts import assert_equal
+from proboscis.asserts import assert_false
+from proboscis.asserts import assert_raises
+from proboscis.asserts import assert_true
+from proboscis import after_class
+from proboscis import before_class
+from proboscis import SkipTest
+from proboscis import test
+
+
+def test_user_config():
+    config = load_user_config()
+    with Check as check:
+        check.true(isinstance(config.user, str))
+
+
+db_config = {
+    "url": "test.db.mycompany.com",
+    "user": "service_admin",
+    "password": "pass"
+}
+
+
+
+@test
+def initialize_database():
+    """Creates a local database."""
+    mymodule.create_database()
+    assert_true(mymodule.tables_exist())
+
+@test(depends_on=[initialize_database])
+def initialize_web_server():
+    """Starts up the web service."""
+    mymodule.start_web_server()
+    admin = mymodule.get_admin_client()
+    assert_true(admin.service_is_up)
+
+
+class UserTests(object):
+
+    @staticmethod
+    def generate_new_user_config():
+        """Constructs the dictionary needed to make a new user."""
+        new_user_config = {
+            "username": "TEST_%s_%s" % (datetime.now(), random.randint(0, 256)),
+            "password": "password",
+            "type":"normal"
+        }
+        return new_user_config
+
+    @before_class
+    def create_user(self):
+        """Create a user."""
+        random.seed()
+        global test_user
+        test_user = None
+        new_user_config = self.generate_new_user_config()
+        admin = mymodule.get_admin_client()
+        self.test_user = admin.create_user(new_user_config)
+        assert_equal(self.test_user.username, new_user_config["username"])
+        assert_true(self.test_user.id is not None)
+        assert_true(isinstance(self.test_user.id, (types.IntType,
+                                                   types.LongType)))
+
+    @after_class(always_run=True)
+    def delete_user(self):
+        if self.test_user is None:
+            raise SkipTest("User tests were never run.")
+        admin = mymodule.get_admin_client()
+        admin.delete_user(self.test_user.id)
+        assert_raises(mymodule.UserNotFoundException, mymodule.login,
+                    {'username':self.test_user.username, 'password':'password'})
+
+    def cant_login_with_wrong_password(self):
+        assert_raises(mymodule.UserNotFoundException, mymodule.login,
+                      {'username':self.test_user.username, 'password':'blah'})
+
+    @test
+    def successful_login(self):
+        self.client = mymodule.login({
+            'username':self.test_user.username, 'password':'password'})
+
+    @test(depends_on=[successful_login])
+    def change_profile_image(self):
+        """Test changing a client's profile image."""
+        assert_equal("default.jpg", self.client.get_profile_image())
+        self.client.set_profile_image("spam.jpg")
+        assert_equal("spam.jpg", self.client.get_profile_image())
+
+
+@test(groups=["user", "service.tests"])
+class NormalUserTests(UserTests):
+
+    @test(depends_on=[UserTests.successful_login])
+    def a_normal_user_cant_create_users(self):
+        """Make sure the given client cannot perform admin actions.."""
+        assert_raises(mymodule.AuthException, self.client.create_user,
+                      self.generate_new_user_config())
+
+    @test(depends_on=[UserTests.successful_login])
+    def a_normal_user_cant_delete_users(self):
+        """Make sure the given client cannot perform admin actions.."""
+        assert_raises(mymodule.AuthException, self.client.delete_user,
+                      self.test_user.id)
+
+
+
+@test(groups=["user", "service.tests"])
+class AdminUserTests(UserTests):
+
+    @staticmethod
+    def generate_new_user_config():
+        """Constructs the dictionary needed to make a new user."""
+        new_user_config = {
+            "username": "TEST_%s_%s" % (datetime.now(), random.randint(0, 256)),
+            "password": "password",
+            "type":"admin"
+        }
+        return new_user_config
+
+    @test(depends_on=[UserTests.successful_login])
+    def an_admin_user_can_create_users(self):
+        """Make sure the given client cannot perform admin actions.."""
+        self.new_user = self.client.create_user(self.generate_new_user_config())
+        # Make sure it actually logs in.
+        self.new_user_client = mymodule.login({
+            'username':self.new_user.username, 'password':'password'})
+
+    @test(depends_on=[an_admin_user_can_create_users])
+    def an_admin_user_can_delete_users(self):
+        """Make sure the given client cannot perform admin actions.."""
+        self.client.delete_user(self.new_user.id)
+
+
+# Add more tests in the service.tests group here, or in any other file.
+# Then when we're finished...
+
+@test(groups=["service.shutdown"], depends_on_groups=["service.tests"],
+      always_run=True)
+def shut_down():
+    """Shut down the web service and destroys the database."""
+    admin = mymodule.get_admin_client()
+    if admin.service_is_up:
+        mymodule.stop_web_server()
+        assert_false(admin.service_is_up())
+    mymodule.destroy_database()
+
