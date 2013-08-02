e2c606a223c12c955eb86620ed4dd362097694bc
Tim Simpson <tim.simpson@rackspace.com>
2013-08-02 10:51:15 -0500

    Adds support for Subunit.

---
+++ examples/parallel_deps/run_tests.py
@@ -0,0 +1,21 @@
+from proboscis import register
+from proboscis import TestProgram
+
+def run_tests():
+    from tests import service_tests
+
+    # Now create some groups of groups.
+    register(groups=["fast"], depends_on_groups=["unit"])
+    register(groups=["integration"],
+             depends_on_groups=["service.initialize",
+                                "service.tests",
+                                "service.shutdown"])
+    register(groups=["slow"],
+             depends_on_groups=["fast", "integration"])
+
+    # Run Proboscis and exit.
+    TestProgram().run_and_exit()
+
+
+if __name__ == '__main__':
+    run_tests()
